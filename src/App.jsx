aec972c1db5e3c99a812bff9b222f06bed220c35
Reevss <0217.raven.ringel@gmail.com>
2023-08-03 16:53:18 +0800

    edited route path in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,7 @@ function App() {
       <Router>
         <NavBar />
         <Routes>
-          {/* <Route exact path="/" element={<Home />}></Route> */}
-          <Route exact path="/Animeesh" element={<Home />}></Route>
+          <Route path="/" element={<Home />}></Route>
           <Route path="/popular" element={<MostPopular />}></Route>
           <Route path="/genres" element={<Genres />}></Route>
           <Route path="/watch" element={<WatchAnime />}></Route>
