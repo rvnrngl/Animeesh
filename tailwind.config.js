84b5305b55d50657798bb4ba7629625a71ef580f
Reevss <0217.raven.ringel@gmail.com>
2023-08-04 19:51:20 +0800

    added custom breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
+    screens: {
+      xs: "400px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
     extend: {},
   },
   plugins: [],
