b1fa9c0bfb6af339900364c4bbc589e2021a5c5c
Reevss <0217.raven.ringel@gmail.com>
2023-08-06 22:54:57 +0800

    bring back HashRouter :)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Genres } from "./pages/Genres";
 import { NavBar } from "./components/NavBar";
