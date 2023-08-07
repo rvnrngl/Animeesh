6cd7bd7f784670d20a2332ca0b81e1f56ca008b8
Reevss <0217.raven.ringel@gmail.com>
2023-08-07 17:50:33 +0800

    added fetch data from api and pass it in Cards component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Cards } from "../components/Cards";
 import { Carousel } from "../components/Carousel";
 import { Trending } from "../components/Trending";
 
+import { META } from "@consumet/extensions";
+
 export const Home = () => {
+  const anilist = new META.Anilist();
+  const [recentAnime, setRecentAnime] = useState([]);
+
+  //get recent anime episodes
+  const getRecentAnime = async () => {
+    await anilist.fetchRecentEpisodes().then((data) => {
+      setRecentAnime(data.results);
+    });
+  };
+
+  useEffect(() => {
+    getRecentAnime();
+  }, []);
+
   return (
     <div className="w-screen min-h-screen dark:bg-zinc-900">
       <div className="w-full flex flex-col justify-center items-center gap-3 px-3 lg:px-5">
@@ -24,7 +40,7 @@ export const Home = () => {
             Recent Updated
           </h1>
           <div className="w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-3 lg:gap-4">
-            <Cards type={"recent"} />
+            <Cards animeList={recentAnime} type={"recent"} />
           </div>
         </div>
       </div>
