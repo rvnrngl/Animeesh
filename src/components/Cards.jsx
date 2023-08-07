92bddba9b9951bf1d7b5c114034b293aec9495fa
Reevss <0217.raven.ringel@gmail.com>
2023-08-07 17:57:32 +0800

    updated Card component design

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,49 +1,60 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
-export const Cards = () => {
-  const [recentAnime, setRecentAnime] = useState([]);
+import { FaPlay } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
-  //get recent anime episodes
-  const url = "https://api.consumet.org/anime/gogoanime/recent-episodes";
-  const getRecentAnime = async () => {
-    try {
-      const response = await axios.get(url, { params: { page: 1, type: 1 } });
-      setRecentAnime(response.data.results);
-      // console.log(response);
-    } catch (err) {
-      throw new Error(err.message);
-    }
+export const Cards = ({ animeList, type }) => {
+  const navigate = useNavigate();
+  const handleNavigation = (anime) => {
+    window.localStorage.setItem("type", type);
+    navigate("/watch", { state: { anime } });
   };
-
-  useEffect(() => {
-    getRecentAnime();
-  }, []);
-
   return (
     <>
-      {recentAnime.map((anime) => {
+      {animeList.map((anime) => {
         return (
           <div
             key={anime.id}
-            className="flex flex-col items-center text-sm lg:text-base rounded-md overflow-hidden
-            lg:hover:brightness-75 duration-300 ease-out"
+            onClick={() => handleNavigation(anime)}
+            className="flex flex-col items-center text-sm lg:text-base rounded-sm overflow-hidden group cursor-pointer"
           >
-            <div className="h-[87%] w-full rounded-lg overflow-hidden object-cover relative">
+            <div className="h-full w-full rounded-sm overflow-hidden object-cover relative">
               <img
                 src={anime.image}
-                alt={anime.title}
+                alt={anime.title?.english}
                 className="h-full w-full object-cover object-center"
               />
-              {/* hover details */}
-              {/* <div className="absolute w-full h-1/4 bottom-0 left-0 bg-black/50 text-white text-sm">
-                <p className="text-lg">Episode: {anime.episodeNumber}</p>
-              </div> */}
+              {/* hover */}
+              <div
+                className="bg-transparent group-hover:bg-zinc-900/50 absolute top-0 left-0 w-full h-full 
+              flex justify-center items-center transition-all duration-200"
+              >
+                <FaPlay
+                  className=" text-transparent group-hover:text-orange-500 text-3xl lg:text-4xl group-hover:block 
+                transition-all duration-200"
+                />
+              </div>
             </div>
-
-            <p className="h-[13%] w-full text-center dark:text-gray-300">
-              {anime.title}
-            </p>
+            <div className="w-full text-left dark:text-gray-300 py-2 flex flex-col">
+              <span className="text-white text-sm font-semibold">
+                {anime.title?.english === null
+                  ? anime.title?.userPreferred?.length > 20
+                    ? anime.title?.userPreferred.slice(0, 20) + "..."
+                    : anime.title?.userPreferred
+                  : anime.title?.english?.length > 20
+                  ? anime.title?.english.slice(0, 20) + "..."
+                  : anime.title?.english}
+              </span>
+              <div className=" flex gap-2 text-sm text-gray-400">
+                <span>{anime.type}</span>
+                <span>•</span>
+                {type === "recent" ? (
+                  <span>Episode: {anime.episodeNumber}</span>
+                ) : (
+                  <span>EPS: {anime.totalEpisodes}</span>
+                )}
+              </div>
+            </div>
           </div>
         );
       })}
