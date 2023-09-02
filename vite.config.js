f8c635de944b227b787559ea4ba71db9411299c1
Reevss <0217.raven.ringel@gmail.com>
2023-09-02 18:15:23 +0800

    rewrites path

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,55 @@
+// import path from "path";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+//   server: {
+//     proxy: {
+//       // Add the proxy middleware to the Vite server
+//       "/api/enime": {
+//         target: "https://api.enime.moe",
+//         changeOrigin: true,
+//         secure: false,
+//         ws: true,
+//         rewrite: (path) => path.replace(/^\/api\/enime/, ""),
+//       },
+//     },
+//   },
+// });
+
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    proxy: {
-      // Add the proxy middleware to the Vite server
-      "/api/enime": {
-        target: "https://api.enime.moe",
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-        rewrite: (path) => path.replace(/^\/api\/enime/, ""),
+export default ({ command }) => {
+  const isDevelopment = command === "serve";
+
+  return defineConfig({
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-  },
-});
+    server: {
+      proxy: isDevelopment
+        ? {
+            "/api/enime": {
+              target: "https://api.enime.moe",
+              changeOrigin: true,
+              secure: false,
+              ws: true,
+              rewrite: (path) => path.replace(/^\/api\/enime/, ""),
+            },
+          }
+        : {},
+    },
+  });
+};
