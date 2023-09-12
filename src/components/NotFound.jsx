8dfc8b69bc8d99695f20700b2b05f6893e6c70d5
Reevss <0217.raven.ringel@gmail.com>
2023-09-12 18:16:50 +0800

    added error page

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+export const NotFound = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-7">
+      <h1 className="text-5xl font-bold">Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p className="text-gray-500 hover:text-orange-400/80">
+        <i onClick={() => navigate(-1)} className="cursor-pointer underline">
+          Go Back
+        </i>
+      </p>
+    </div>
+  );
+};
