d1ca8a60c5c3c133dd816af6b1a0c472f1260085
“rvnrngl” <0217.raven.ringel@gmail.com>
2024-05-18 21:55:30 +0800

    added fix to services not working

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.PROD
-  ? import.meta.env.VITE_API_PROD
-  : import.meta.env.VITE_API_LOCAL;
+const BASE_URL = import.meta.env.VITE_API_PROD;
 
 export const apiService = axios.create({
   baseURL: BASE_URL,
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.PROD
-  ? import.meta.env.VITE_API_USER_PROD
-  : import.meta.env.VITE_API_USER_LOCAL;
+const BASE_URL = import.meta.env.VITE_API_USER_PROD;
 
 export const userService = axios.create({
   baseURL: BASE_URL,
