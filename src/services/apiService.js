d6637e67f705dd9900788219aef88b2e8464b4bb
“rvnrngl” <0217.raven.ringel@gmail.com>
2024-05-18 22:05:07 +0800

    added dynamic base_url based in development mode

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_PROD;
+const BASE_URL =
+  import.meta.env.MODE === "production"
+    ? import.meta.env.VITE_API_PROD
+    : import.meta.env.VITE_API_LOCAL;
 
 export const apiService = axios.create({
   baseURL: BASE_URL,
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_USER_PROD;
+const BASE_URL =
+  import.meta.env.MODE === "production"
+    ? import.meta.env.VITE_API_USER_PROD
+    : import.meta.env.VITE_API_USER_LOCAL;
 
 export const userService = axios.create({
   baseURL: BASE_URL,
